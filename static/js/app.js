272a1b73e47816fe1a9ab3e0c27d95ba9219c6d0
Erikcaldwell <39847875+Erikcaldwell@users.noreply.github.com>
2018-10-06 15:36:52 -0700

    Repair
    updating the static.js and backing out my attempts to change the CSS

--- static/js/app.js
+++ static/js/app.js
@@ -1,76 +1,83 @@
-function buildMetadata(sample) {
+// @TODO: Complete the following function that builds the metadata panel
 
-  // @TODO: Complete the following function that builds the metadata panel
+// Use `d3.json` to fetch the metadata for a sample
+  // Use d3 to select the panel with id of `#sample-metadata`
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  // Use `.html("") to clear any existing metadata
 
-    // Use `.html("") to clear any existing metadata
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+  // BONUS: Build the Gauge Chart
+  // buildGauge(data.WFREQ);
+
+  function buildMetadata(sample) {
     d3.json(`/metadata/${sample}`).then((data) => {
       var PANEL = d3.select("#sample-metadata");
       PANEL.html("");
       Object.entries(data).forEach(([key, value]) => {
         PANEL.append("h6").text(`${key}: ${value}`);
       });
-    // BONUS: Build the Gauge Chart
 
-}
+      // BONUS: Build the Gauge Chart
+      buildGauge(data.WFREQ);
+    });
+  }
+
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   d3.json(`/samples/${sample}`).then((data) => {
-    var otu_ids = data.otu_ids;
-    var otu_labels = data.otu_labels;
-    var sample_values = data.sample_values;
+    var ids = data.otu_ids;
+    var labels = data.otu_labels;
+    var values = data.sample_values;
+
 
     // @TODO: Build a Bubble Chart using the sample data
-    var bLayout = {
+    var LayoutBubble = {
       margin: { t: 0 },
+      xaxis: { title: "Id's" }
       hovermode: "closest",
-      xaxis: { title: "OTU ID" }
-    };
-    var bData = [
+      };
+
+      var DataBubble = [
       {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
+        x: ids,
+        y: values,
+        text: labels,
         mode: "markers",
         marker: {
-          size: sample_values,
-          color: otu_ids,
-          colorscale: "Viridis"
-        }
+          color: ids,
+          size: values,
+          }
       }
     ];
-// ploting the layout and data
-    Plotly.plot("bubble", bData, bLayout);
+
+    Plotly.plot("bubble", DataBubble, LayoutBubble);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-    var pData = [
+
+    var pie_data = [
       {
-        values: sample_values.slice(0, 10),
-        labels: otu_ids.slice(0, 10),
-        hovertext: otu_labels.slice(0, 10),
+        values: values.slice(0, 10),
+        labels: ids.slice(0, 10),
+        hovertext: labels.slice(0, 10),
         hoverinfo: "hovertext",
         type: "pie"
       }
     ];
 
-    var pLayout = {
+    var pie_layout = {
       margin: { t: 0, l: 0 }
     };
 
-    Plotly.plot("pie", pData, pLayout);
+    Plotly.plot("pie", pie_data, pie_layout);
   });
 }
-
 
 function init() {
   // Grab a reference to the dropdown select element
